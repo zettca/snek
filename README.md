fb19f9766a613841587eb9cfb6e4ff35111813c0
Bruno <zettca@gmail.com>
2018-01-24 00:09:45 +0000

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Snek
-
-A multiplayer Snake game
--- client/src/settings.js
+++
@@ -1,8 +0,0 @@
-export const gameSettings = {
-  tickTime: 100, // ms
-  tileCount: { x: 10, y: 10 },
-};
-
-export const canvasSettings = {
-  canvas: { width: 300, height: 300 },
-};
