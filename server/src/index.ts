f6736376ab0ceda1520d322b8dd6fbc03e8d8d4b
Bruno <zettca@gmail.com>
2021-03-20 23:51:11 +0000

    🛠fallback to room1

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,10 +15,11 @@ const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 // game rooms setup
-const socketRooms: Record<string, string> = {};
+const socketRooms: Record<string, SnekGame> = {};
 const gameRooms: Record<string, SnekGame> = {
   room1: new SnekGame({ tickTime: 200, tiles: { x: 12, y: 12 }, apples: 1 }),
   room2: new SnekGame({ tickTime: 100, tiles: { x: 20, y: 20 }, apples: 2 }),
+  room3: new SnekGame({ tickTime: 100, tiles: { x: 32, y: 32 }, apples: 4 }),
 };
 
 for (const roomId in gameRooms) {
@@ -35,21 +36,22 @@ for (const roomId in gameRooms) {
 
 io.on("connection", (socket) => {
   socket.on("join", (roomId: string) => {
-    const snekGame = gameRooms[roomId];
-    socketRooms[socket.id] = roomId;
+    const room = gameRooms.hasOwnProperty(roomId) ? roomId : "room1";
+    const snekGame = gameRooms[room];
+    socketRooms[socket.id] = snekGame;
     snekGame.addSnake(socket.id);
 
-    socket.join(roomId || "room1");
+    socket.join(room);
     socket.emit("config", snekGame?.getConfig());
   });
 
   socket.on("input", (data) => {
-    const snekGame = gameRooms[socketRooms[socket.id]];
+    const snekGame = socketRooms[socket.id];
     snekGame?.sendDirection(socket.id, data);
   });
 
   socket.on("disconnect", () => {
-    const snekGame = gameRooms[socketRooms[socket.id]];
+    const snekGame = socketRooms[socket.id];
     snekGame?.removeSnake(socket.id);
     delete socketRooms[socket.id];
   });
